d16006bfbec2055eda74e9844092890473fc0c59
subrata003 <subratajpg2003@gmail.com>
2025-10-03 15:51:55 +0530

    COMMIT

--- Constraints_in_sql.sql
+++ Constraints_in_sql.sql
@@ -13,4 +13,24 @@ VALUES
 -- (123),  -- duplicate entry is not allow becouse of unique 
 (125);
 
-SELECT * FROM student;
+USE collage;
+-- not null constraint
+CREATE TABLE student1(
+age INT,
+rollno INT NOT NULL
+
+);
+-- INSERT INTO student1(age) -- that is not work becouse in rollno there is no value insert means null so i create rollno as not null 
+INSERT INTO student1(age,rollno)
+VALUES
+(22,01),
+(21,02);
+
+-- check constraint
+CREATE TABLE student2(
+age INT CHECK(age>18)
+);
+INSERT INTO student2(age)
+VALUES (22);
+
+
